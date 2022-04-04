fa3631961b9b7e031aed4e16c10d9351d2432386
Sule <aybilenden@gmail.com>
2022-04-04 23:11:56 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Footer from "./components/Footer";
@@ -16,12 +16,17 @@ function App() {
         <Navbar />
         <div className="main">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="products/details/:productId" element={<Details />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path="/home" element={<Home />} />
+            {/* <Route path="/" element={<Navigate replace to="/home" />} /> */}
+            <Route path="/about" exact element={<About />} />
+            <Route path="/products" exact element={<Products />} />
+            <Route
+              path="products/details/:productId"
+              exact
+              element={<Details />}
+            />
+            <Route path="/checkout" exact element={<Checkout />} />
+            <Route path="*" exact element={<Home />} />
           </Routes>
         </div>
         <Footer />
