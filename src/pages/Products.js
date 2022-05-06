5b0b444bcdae2b3b210555fc84310cd84a0ffd67
Sule <aybilenden@gmail.com>
2022-05-06 22:03:04 +0300

    done

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Product.module.css";
+import spinner from "../assets/wait3.gif";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -14,19 +15,23 @@ const Products = () => {
   return (
     <div className={styles.productContainer}>
       <h2>Product Page</h2>
-      <div className={styles.products}>
-        {products.map((each) => (
-          <Link
-            to={`details/${each.id}`}
-            key={each.id}
-            className={styles.product}
-          >
-            <h4>{each.category}</h4>
-            <p>{each.title}</p>
-            <img style={{ width: "100px" }} src={each.image} alt="" />
-          </Link>
-        ))}
-      </div>
+      {products ? (
+        <div className={styles.products}>
+          {products?.map((each) => (
+            <Link
+              to={`details/${each.id}`}
+              key={each.id}
+              className={styles.product}
+            >
+              <h4>{each.category}</h4>
+              <p>{each.title}</p>
+              <img style={{ width: "100px" }} src={each.image} alt="" />
+            </Link>
+          ))}
+        </div>
+      ) : (
+       <div><img src={spinner} alt="spinner" /></div>
+      )}
     </div>
   );
 };
